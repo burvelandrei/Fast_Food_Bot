7140f209aa0b69b68f2042442be67e474d7e1dc4
Andrei Burvel <burvelandrei@gmail.com>
2025-03-06 01:50:31 +0300

    V 1.8 добавлена колонка email в таблице user

--- db/models.py
+++ db/models.py
@@ -13,7 +13,8 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    tg_id: Mapped[str] = mapped_column(unique=True, nullable=True)
-
+    tg_id: Mapped[str] = mapped_column(unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    
     def __repr__(self):
         return str(self.id)
