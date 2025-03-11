2626eb41e13b76af834ffe95cf4aa111b654ff49
Andrei Burvel <burvelandrei@gmail.com>
2025-03-11 15:47:12 +0300

    V 1.34 добавлена функция для прочтения очереди rabbitmq на предмет подтверждённых почт, в requirements добавлена библиотека для работы c rabbitmq

---
+++ utils/rmq_consumer.py
@@ -0,0 +1,33 @@
+import aio_pika
+import json
+from aiogram import Bot
+from aiogram.types import Chat, User
+from aiogram.types import Message
+from aiogram_dialog import DialogManager
+from environs import Env
+from db.operations import UserDO
+from dialogs.states import MenuSG
+
+env = Env()
+env.read_env()
+
+
+# Функция для прочтения очереди rabbitmq на предмет подтверждённых почт
+async def listen_for_confirmations(bot: Bot, session):
+    connection = await aio_pika.connect_robust(
+        f"amqp://{env('RMQ_USER')}:{env('RMQ_PASSWORD')}@localhost/"
+    )
+    channel = await connection.channel()
+
+    queue = await channel.declare_queue("user_confirmations")
+
+    async with queue.iterator() as queue_iter:
+        async for message in queue_iter:
+            async with message.process():
+                event_data = json.loads(message.body)
+                tg_id = event_data.get("tg_id")
+                email = event_data.get("email")
+
+                if tg_id:
+                    await UserDO.add(session=session, **{"tg_id": tg_id, "email": email})
+                    await bot.send_message(tg_id, f"✅ Ваша почта {email} успешно подтверждена!")
